763716d068383d7cf2a0b86e8b473d2c8dcfa846
MVigil6343 <6343@holbertonstudents.com>
2023-02-21 23:47:51 -0800

    task 7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,16 +8,12 @@
 
 int print_last_digit(int n)
 {
-	int last_digit;
+	int last_digit = n % 10;
 
 	if (n < 0)
-		n = -n;
-	if (n == 0)
 	{
-		_putchar('0');
-		return (0);
+		last_digit = -last_digit;
 	}
-	last_digit = n % 10;
 	_putchar(last_digit + '0');
 	return (last_digit);
 }
