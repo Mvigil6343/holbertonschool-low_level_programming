db923cbd8772eb545f98a5004e727718951ada45
MVigil6343 <6343@holbertonstudents.com>
2023-02-27 10:27:48 -0800

    task 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: the string to print
+ */
+
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
