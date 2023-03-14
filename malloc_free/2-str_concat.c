d94de5c02f964f78f7ff2029544b435a2d275a1f
MVigil6343 <6343@holbertonstudents.com>
2023-03-14 12:24:41 -0700

    task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,11 +14,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		s1 = " ";
+		s1 = "";
 	}
 	if (s2 == NULL)
 	{
-		s2 = " ";
+		s2 = "";
 	}
 
 	str = malloc(sizeof(char) * (strlen(s1) + strlen(s2)) + 1);
