4d5ba5b84cc7083627ca0d32d687aa72d60c444b
Mvigil6343 <6343@holbertonstudents.com>
2023-06-15 15:43:49 -0700

    task_5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ * Return: 0
+ */
+int main(void)
+{
+	int num = 0;
+
+	for (; num <= 9; num++)
+		printf("%d", num);
+	putchar('\n');
+	return (0);
+}
