332ed050403ebcf2e604c22701c077b761ae6aa2
Mvigil6343 <6343@holbertonstudents.com>
2023-06-18 23:19:10 -0700

    Task_2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,18 @@
 #include "main.h"
 /**
- * print_alphabet - prints the alphabet
+ * print_alphabet_x10 - prints the alphabet
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	char letter;
 	int i = 0;
 
 	for (; i < 10; i++)
+	{
 		for (letter = 'a'; letter <= 'z'; letter++)
+		{
 			_putchar(letter);
-	_putchar('\n');
+		}
+		_putchar('\n');
+	}
 }
