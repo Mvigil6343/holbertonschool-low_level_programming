ffe6537f0acee03cf0d4e5158da1fe2e83b9b517
Mvigil6343 <6343@holbertonstudents.com>
2023-07-07 11:20:58 -0700

    Header_file

---
+++ more_malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef __main_h__
+#define __main_h__
+
+#include <stdlib.h>
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+
+#endif
