ccf86c4cef275cd6fa8cdf8278c21c297f072d7d
Mvigil6343 <6343@holbertonstudents.com>
2023-08-02 13:02:30 -0700

    added libraries

--- file_io/main.h
+++ file_io/main.h
@@ -1,5 +1,10 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
